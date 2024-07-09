1c8cd67c85a2bd309db0f546da6a858cb5070173
Simon Velichko <simon.velichko@rallyware.com>
2024-07-09 12:05:20 +0300

    use certificate_authority in kubernetes provider

--- components/terraform/wp/providers.tf
+++ components/terraform/wp/providers.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  config_path    = "~/.kube/config"
-  config_context = local.eks_cluster_arn
+  host = data.aws_eks_cluster.my-staging-ue2.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.my-staging-ue2.certificate_authority[0].data)
 }
